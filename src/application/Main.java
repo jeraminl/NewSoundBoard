66c2aa10b4b554254647fcfff19db91fce7c1b25
jeraminl <32972952+jeraminl@users.noreply.github.com>
2018-09-13 18:50:36 -0700

    added a play button

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,11 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.media.MediaPlayer;
 
@@ -14,18 +17,32 @@ public class Main extends Application {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			Button button = new Button("play");
+			Scene buttonScene = new Scene(button, 200, 100);
+			
+			button.setOnAction(new EventHandler<ActionEvent>() {
+				public void handle(ActionEvent actionEvent) {
+					SoundEntity test = new SoundEntity("src/gunshot.wav");
+					MediaPlayer mediaPlayer = new MediaPlayer(test.clip);
+					
+					mediaPlayer.play();
+				}
+			});
+			
+			
 			primaryStage.setScene(scene);
+			primaryStage.setScene(buttonScene);
 			primaryStage.show();
+			
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public static void main(String[] args) {
-		SoundEntity test = new SoundEntity("src/gunshot.wav");
-		MediaPlayer mediaPlayer = new MediaPlayer(test.clip);
-		
-		mediaPlayer.play();
+
 		launch(args);
 
 	}
